e4a94f06ca1229b23fe6bf4a62572be99eecf95b
Nicola Gigante <nicola.gigante@gmail.com>
2026-09-04 22:30:18 +0100

    First code for the decision diagrams module
    Signed-off-by: Nicola Gigante <nicola.gigante@gmail.com>

---
+++ crates/dd/src/obdd/vars.rs
@@ -0,0 +1,166 @@
+//
+// ::formally - the open-source formal methods toolchain
+//
+// Copyright (c) 2026 Nicola Gigante
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+//
+
+use std::{num::NonZero, ops::Not};
+
+#[derive(Clone, Copy, Hash, PartialEq, Eq)]
+pub struct Var(pub(super) NonZero<u32>);
+
+impl Not for Var {
+    type Output = Lit;
+
+    fn not(self) -> Lit {
+        !Lit::from(self)
+    }
+}
+
+#[derive(Clone, Copy, Hash, PartialEq, Eq)]
+pub struct Lit(NonZero<i32>);
+
+impl From<Var> for Lit {
+    fn from(var: Var) -> Self {
+        Lit(var.0.cast_signed())
+    }
+}
+
+impl Not for Lit {
+    type Output = Lit;
+
+    fn not(self) -> Lit {
+        Lit(-self.0)
+    }
+}
+
+impl Lit {
+    pub fn var(self) -> Var {
+        Var(self.0.unsigned_abs())
+    }
+
+    pub fn value(self) -> bool {
+        self.0.is_positive()
+    }
+}
+
+#[derive(Default, Clone, Copy, Hash, PartialEq, Eq, PartialOrd, Ord)]
+pub struct Position(u32);
+
+#[derive(Clone, Copy)]
+struct Entry {
+    position: Position,
+    subsequent: Position,
+}
+
+impl Default for Entry {
+    fn default() -> Self {
+        Entry {
+            position: Position(0),
+            subsequent: Position(u32::MAX),
+        }
+    }
+}
+
+#[derive(Clone)]
+pub struct Order {
+    entries: Vec<Entry>,
+    max: Option<Var>,
+}
+
+impl Default for Order {
+    fn default() -> Self {
+        Order {
+            entries: vec![Entry::default()],
+            max: None,
+        }
+    }
+}
+
+impl Order {
+    pub fn new() -> Order {
+        Order::default()
+    }
+
+    pub fn var_after(&mut self, previous: impl Into<Option<Var>>) -> Var {
+        let previous = self.entry_mut(previous.into());
+
+        let position =
+            Position(previous.position.0 + ((previous.subsequent.0 - previous.position.0) / 2));
+
+        assert!(position > previous.position);
+
+        let subsequent = previous.subsequent;
+
+        previous.subsequent = position;
+
+        let var = Var(NonZero::new(self.entries.len() as u32).unwrap());
+        let entry = Entry {
+            position,
+            subsequent,
+        };
+        self.entries.push(entry);
+
+        if entry.position > self.entry(self.max).position {
+            self.max = Some(var);
+        }
+
+        var
+    }
+
+    pub fn var(&mut self) -> Var {
+        self.var_after(self.max)
+    }
+
+    pub fn position(&self, var: Var) -> Position {
+        self.entry(Some(var)).position
+    }
+
+    fn entry_mut(&mut self, var: Option<Var>) -> &mut Entry {
+        match var {
+            None => &mut self.entries[0],
+            Some(var) => &mut self.entries[var.0.get() as usize],
+        }
+    }
+
+    fn entry(&self, var: Option<Var>) -> Entry {
+        match var {
+            None => self.entries[0],
+            Some(var) => self.entries[var.0.get() as usize],
+        }
+    }
+}
+
+#[test]
+pub fn order() {
+    let mut order = Order::new();
+    let first = order.var();
+    let second = order.var_after(first);
+
+    assert!(order.position(first) < order.position(second));
+
+    let middle = order.var_after(first);
+    assert!(order.position(first) < order.position(middle));
+    assert!(order.position(middle) < order.position(second));
+
+    let max = order.var();
+    assert!(order.position(second) < order.position(max));
+}
