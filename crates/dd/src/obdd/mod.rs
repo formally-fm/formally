41328deee5019f7ed438426369471a2335557a41
Nicola Gigante <nicola.gigante@gmail.com>
2026-09-16 10:58:19 +0200

    Better variable swap, restrict(), exists(), forall()
    Signed-off-by: Nicola Gigante <nicola.gigante@gmail.com>

--- crates/dd/src/obdd/mod.rs
+++ crates/dd/src/obdd/mod.rs
@@ -31,11 +31,12 @@ use itertools::Itertools;
 use parking_lot::RwLock;
 
 use std::{
-    cmp,
+    cmp::{self, max, min},
     fmt::{Debug, Formatter},
     ops::{BitAnd, BitAndAssign, BitOr, BitOrAssign, BitXor, BitXorAssign, Not},
 };
 
+pub use inner::Level;
 use inner::{Inner, SlotID, VarID};
 
 #[derive(Clone, Copy, Hash, PartialEq, Eq)]
@@ -56,12 +57,15 @@ impl<'m> Var<'m> {
         self.manager.into_inner()
     }
 
+    pub fn level(&self) -> Level {
+        self.manager.inner.read().level(Some(self.var))
+    }
+
     pub fn next(&self) -> Option<Var<'m>> {
-        self.manager
-            .inner
-            .read()
-            .next(self.var)
-            .map(|v| Var::new(v, self.manager()))
+        let inner = self.manager.inner.read();
+        inner
+            .at_level(inner.level(Some(self.var)) + 1)
+            .map(|id| Var::new(id, self.manager()))
     }
 }
 
@@ -127,8 +131,32 @@ impl Manager {
         vars
     }
 
-    pub fn swap(&self, var: Var<'_>) {
-        self.inner.write().swap(var.var);
+    pub fn swap_adjacent(&self, var: Var<'_>) {
+        let mut inner = self.inner.write();
+        let level = inner.level(Some(var.var));
+        inner.swap(level);
+    }
+
+    pub fn swap(&self, v1: Var<'_>, v2: Var<'_>) {
+        if v1 == v2 {
+            return;
+        }
+
+        let mut inner = self.inner.write();
+        let l1 = inner.level(Some(v1.var));
+        let l2 = inner.level(Some(v2.var));
+
+        let min = min(l1, l2);
+        let max = max(l1, l2);
+        let distance = (max - min) as u32;
+
+        for i in 0..distance {
+            inner.swap(min + i)
+        }
+
+        for i in (0..distance - 1).rev() {
+            inner.swap(min + i)
+        }
     }
 
     pub fn top(&self) -> BDD<'_> {
@@ -161,6 +189,32 @@ impl Manager {
 
     pub fn implies<'m>(&'m self, left: impl Into<BDD<'m>>, right: impl Into<BDD<'m>>) -> BDD<'m> {
         self.ite(left, right, self.top())
+    }
+
+    pub fn exists<'m>(&'m self, var: Var<'m>, body: impl Into<BDD<'m>>) -> BDD<'m> {
+        let body = body.into();
+        self.or([self.restrict(var, &body), self.restrict(!var, body)])
+    }
+
+    pub fn forall<'m>(&'m self, var: Var<'m>, body: impl Into<BDD<'m>>) -> BDD<'m> {
+        self.not(self.exists(var, self.not(body)))
+    }
+
+    pub fn restrict<'m>(&'m self, lit: impl Into<Lit<'m>>, body: impl Into<BDD<'m>>) -> BDD<'m> {
+        let lit = lit.into();
+        let body = body.into();
+        assert_eq!(
+            Nominal(lit.manager()),
+            Nominal(body.manager()),
+            "restrict() called on Lit and BDD from different managers"
+        );
+
+        let restrict = self
+            .inner
+            .read()
+            .restrict(lit.var().var, lit.value(), body.id);
+
+        BDD::new(restrict, lit.manager())
     }
 
     pub fn ite<'m>(
@@ -196,10 +250,57 @@ pub fn ite<'m>(
     guard.manager.ite(guard, then, else_)
 }
 
+pub fn restrict<'m>(lit: impl Into<Lit<'m>>, body: impl Into<BDD<'m>>) -> BDD<'m> {
+    let lit = lit.into();
+    lit.manager().restrict(lit, body)
+}
+
+pub fn exists<'m>(vars: impl IntoIterator<Item = Var<'m>>, body: impl Into<BDD<'m>>) -> BDD<'m> {
+    vars.into_iter()
+        .fold(body.into(), |acc, var| var.manager().exists(var, acc))
+}
+
+pub fn forall<'m>(vars: impl IntoIterator<Item = Var<'m>>, body: impl Into<BDD<'m>>) -> BDD<'m> {
+    vars.into_iter()
+        .fold(body.into(), |acc, var| var.manager().forall(var, acc))
+}
+
 pub fn implies<'m>(left: impl Into<BDD<'m>>, right: impl Into<BDD<'m>>) -> BDD<'m> {
     let left = left.into();
     let right = right.into();
     left.manager.implies(left, right)
+}
+
+#[derive(Clone, Copy, Hash, PartialEq, Eq)]
+pub enum Lit<'m> {
+    Positive(Var<'m>),
+    Negative(Var<'m>),
+}
+
+impl<'m> Lit<'m> {
+    pub fn var(&self) -> Var<'m> {
+        match self {
+            Lit::Positive(var) => *var,
+            Lit::Negative(var) => *var,
+        }
+    }
+
+    pub fn value(&self) -> bool {
+        match self {
+            Lit::Positive(_) => true,
+            Lit::Negative(_) => false,
+        }
+    }
+
+    pub fn manager(&self) -> &'m Manager {
+        self.var().manager()
+    }
+}
+
+impl<'m> From<Var<'m>> for Lit<'m> {
+    fn from(var: Var<'m>) -> Self {
+        Lit::Positive(var)
+    }
 }
 
 #[derive(Hash, PartialEq, Eq)]
@@ -253,13 +354,38 @@ impl<'m> From<&Var<'m>> for BDD<'m> {
 
 impl<'m> From<Var<'m>> for BDD<'m> {
     fn from(var: Var<'m>) -> Self {
-        let manager = var.manager();
-        let node = manager.inner.read().make(inner::Node {
-            var: var.var,
-            high: SlotID::TOP,
-            low: SlotID::BOTTOM,
-        });
-        BDD::new(node, manager)
+        BDD::from(Lit::from(var))
+    }
+}
+
+impl<'m> From<&Lit<'m>> for BDD<'m> {
+    fn from(lit: &Lit<'m>) -> Self {
+        BDD::from(*lit)
+    }
+}
+
+impl<'m> From<Lit<'m>> for BDD<'m> {
+    fn from(lit: Lit<'m>) -> Self {
+        match lit {
+            Lit::Positive(var) => {
+                let manager = var.manager();
+                let node = manager.inner.read().make(inner::Node {
+                    var: var.var,
+                    high: SlotID::TOP,
+                    low: SlotID::BOTTOM,
+                });
+                BDD::new(node, manager)
+            }
+            Lit::Negative(var) => {
+                let manager = var.manager();
+                let node = manager.inner.read().make(inner::Node {
+                    var: var.var,
+                    high: SlotID::BOTTOM,
+                    low: SlotID::TOP,
+                });
+                BDD::new(node, manager)
+            }
+        }
     }
 }
 
@@ -281,10 +407,10 @@ impl<'m> Not for BDD<'m> {
 }
 
 impl<'m> Not for Var<'m> {
-    type Output = BDD<'m>;
+    type Output = Lit<'m>;
 
     fn not(self) -> Self::Output {
-        self.manager.not(self)
+        Lit::Negative(self)
     }
 }
 
@@ -297,10 +423,29 @@ impl<'m> Not for &BDD<'m> {
 }
 
 impl<'m> Not for &Var<'m> {
-    type Output = BDD<'m>;
+    type Output = Lit<'m>;
 
     fn not(self) -> Self::Output {
-        self.manager.not(self)
+        Lit::Negative(*self)
+    }
+}
+
+impl<'m> Not for Lit<'m> {
+    type Output = Lit<'m>;
+
+    fn not(self) -> Self::Output {
+        match self {
+            Lit::Positive(var) => Lit::Negative(var),
+            Lit::Negative(var) => Lit::Positive(var),
+        }
+    }
+}
+
+impl<'m> Not for &Lit<'m> {
+    type Output = Lit<'m>;
+
+    fn not(self) -> Self::Output {
+        (*self).not()
     }
 }
 
@@ -439,11 +584,15 @@ mod tests {
             assert!(v1 < v2);
         }
 
-        manager.swap(first);
+        manager.swap_adjacent(first);
         assert!(middle < first);
 
-        manager.swap(first);
+        manager.swap_adjacent(first);
         assert!(second < first);
+
+        manager.swap(first, middle);
+        assert!(first < middle);
+        assert!(second < middle);
     }
 
     #[test]
@@ -452,25 +601,47 @@ mod tests {
         let p = manager.var();
         let q = manager.var();
 
-        let tautology = p | !p;
-        let ponens = implies(implies(p, q) & p, q);
-        let not = implies(p, q) & p & !q;
-        let something = p & q;
-        let xor = (p ^ q) & &something;
+        std::thread::scope(|scope| {
+            scope.spawn(|| {
+                let tautology = p | !p;
+                let ponens = implies(implies(p, q) & p, q);
+                let not = implies(p, q) & p & !q;
+                let something = p & q;
+                let xor = (p ^ q) & &something;
+        
+                assert_eq!(tautology, true);
+                assert_eq!(ponens, true);
+                assert_eq!(not, false);
+                assert_ne!(something, true);
+                assert_ne!(something, false);
+                assert_eq!(xor, false);
+            });
 
-        assert_eq!(tautology, true);
-        assert_eq!(ponens, true);
-        assert_eq!(not, false);
-        assert_ne!(something, true);
-        assert_ne!(something, false);
-        assert_eq!(xor, false);
-
-        manager.swap(p);
-
-        let xor = (p ^ q) & &something;
-
-        assert_ne!(something, true);
-        assert_ne!(something, false);
-        assert_eq!(xor, false);
+            scope.spawn(|| {
+                let something = p & q;
+                
+                manager.swap(p, q);
+    
+                let xor = (p ^ q) & &something;
+    
+                assert_ne!(something, true);
+                assert_ne!(something, false);
+                assert_eq!(xor, false);
+            });
+            
+            scope.spawn(|| {
+                let w = manager.var();
+                let something = p & (q | w);
+    
+                manager.swap(p, w);
+    
+                let ep = exists([p], &something);
+                let ap = forall([p], &something);
+    
+                assert_ne!(ep, true);
+                assert_ne!(ep, false);
+                assert_eq!(ap, false);
+            });
+        });
     }
 }
